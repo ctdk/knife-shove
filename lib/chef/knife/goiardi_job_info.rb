b349e1de3d04263a49b549e7f92859421492d62c
Jeremy Bingham <jbingham@gmail.com>
2014-08-17 22:00:19 -0700

    job info for a specific node's run works now

---
+++ lib/chef/knife/goiardi_job_info.rb
@@ -0,0 +1,46 @@
+# @copyright Copyright 2014 Jeremy Bingham.
+# Incorporates software Copyright 2014 Chef Software Inc. All Rights Reserved.
+#
+# This file is provided to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+class Chef
+  class Knife
+    class GoiardiJobInfo < Chef::Knife
+      deps do
+	require 'chef/rest'
+      end
+      banner "knife goiardi job info <job id> <node>"
+
+      def run
+	job_id = @name_args[0]
+	if job_id.nil?
+	  ui.error "No job id specified"
+	  show_usage
+	  exit 1
+	end
+	node = @name_args[1]
+	if node.nil?
+	  ui.error "No node specified"
+	  show_usage
+	  exit 1
+	end
+	rest = Chef::REST.new(Chef::Config[:chef_server_url])
+	info = rest.get_rest("shovey/jobs/#{job_id}/#{node}")
+	output(info)
+      end
+    end
+  end
+end
